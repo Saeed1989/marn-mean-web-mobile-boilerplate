af2b0df09302fbea6f0d5efb0427cf2fae22f384
saeed1 <saeed.sharman@gmail.com>
2021-12-25 19:34:28 +0900

    adds apply micro-service

---
+++ server_apply/src/controllers/routes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const userRoutes = require("./user-controller");
+const roleRoutes = require("./role-controller");
+const catagoryRoutes = require("./catagory-controller");
+const dataRoutes = require("./data-controller");
+const permissionRoutes = require("./permission-controller");
+const filterRoutes = require("./filter-controller");
+const authRoutes = require("./auth-controller");
+
+let router = express.Router();
+
+router.use("/users", userRoutes);
+router.use("/roles", roleRoutes);
+router.use("/catagories", catagoryRoutes);
+router.use("/products", dataRoutes);
+router.use("/permissions", permissionRoutes);
+router.use("/filters", filterRoutes);
+router.use("/auth", authRoutes);
+
+module.exports = router;
