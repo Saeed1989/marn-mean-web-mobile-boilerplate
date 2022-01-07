e47e6baab718a62f819496ab905c3faf4ec3aa20
saeed1 <saeed.sharman@gmail.com>
2022-01-07 18:43:14 +0900

    [server-apply] refactors code base

--- server_apply/src/utils/errors.js
+++ server_apply/src/utils/errors.js
@@ -1,27 +1,33 @@
 class GeneralError extends Error {
-    constructor(message) {
-        super();
-        this.message = message;
-    }
+  constructor(message) {
+    super();
+    this.message = message;
+  }
 
-    getCode() { return 400; }
+  getCode() {
+    return 400;
+  }
 }
 
 class BadRequest extends GeneralError {
-    constructor(message) {
-        super(message);
-        this.name = 'BadRequest';
-    }
-    getCode() { return 400; }
+  constructor(message) {
+    super(message);
+    this.name = "BadRequest";
+  }
+  getCode() {
+    return 400;
+  }
 }
 
 class NotFound extends GeneralError {
-    constructor(message) {
-        super(message);
-        this.name = 'NotFound';
-    }
+  constructor(message) {
+    super(message);
+    this.name = "NotFound";
+  }
 
-    getCode() { return 404; }    
+  getCode() {
+    return 404;
+  }
 }
 
-module.exports = {GeneralError, BadRequest, NotFound}
+module.exports = { GeneralError, BadRequest, NotFound };
